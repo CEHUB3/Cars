d5f0acdb5df0f29e8cd5a58c35d6cb0720ac7c61
CEHUB3 <erikhubbe@gmail.com>
2023-01-10 00:41:36 +0100

    commit

--- src/Cars.java
+++ src/Cars.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Cars {
+import java.sql.SQLOutput;
+
+public class Cars {
+
+    private String brand;
+    private String model;
+    private String color;
+    private int modelYear;
+    static int instanceCounter = 0;
+
+    public Cars(String brand, String model, String color, int modelYear){
+
+        System.out.println("Bara Volvo kan skapas");
+        instanceCounter = instanceCounter + 1;
+
+    }
+    public Cars(){
+
+        this("BMW", "X5", "Blå", 2019);
+        System.out.println("Bara BMW här");
+
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+    public String getModel (){
+        return model;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getModelYear(){
+        return modelYear;
+    }
+
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        boolean running
+        while
+        System.out.print("Välkommen! \nAnge bilmärke: ");
+        String brand = input.nextLine();
+
+        System.out.print("Ange model: ");
+        String model = input.nextLine();
+
+        System.out.print("Ange färg: ");
+        String color = input.nextLine();
+
+        System.out.print("Ange årsmodell: ");
+        int modelYear = input.nextInt();
+        input.nextLine();
+
+        Cars car1 = new Cars("Volvo","V90" , "Vit", 2022);
+        Cars car2 = new Cars("Volvo", "XC60", "Svart", 2020);
+        Cars carUser = new Cars(brand, model, color, modelYear);
+
+
+
+
+
     }
 }
